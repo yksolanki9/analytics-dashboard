8f58aab07ed66d5adebe86fa2ea643ae61e29c12
yksolanki9 <yashsolanki1709@gmail.com>
2023-07-23 18:00:18 +0530

    Remove bar and doughnut chart components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,28 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { MatCardModule } from '@angular/material/card';
-import { DoughnutChartComponent } from './components/doughnut-chart/doughnut-chart.component';
 import { NgChartsModule } from 'ng2-charts';
-import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { StatCardComponent } from './components/stat-card/stat-card.component';
 import { MatIconModule } from '@angular/material/icon';
 import { ChartComponent } from './components/chart/chart.component';
 
 @NgModule({
-  declarations: [
-    CardComponent,
-    DoughnutChartComponent,
-    BarChartComponent,
-    StatCardComponent,
-    ChartComponent,
-  ],
+  declarations: [CardComponent, StatCardComponent, ChartComponent],
   imports: [CommonModule, MatCardModule, NgChartsModule, MatIconModule],
-  exports: [
-    CardComponent,
-    DoughnutChartComponent,
-    BarChartComponent,
-    StatCardComponent,
-    ChartComponent,
-  ],
+  exports: [CardComponent, StatCardComponent, ChartComponent],
 })
 export class SharedModule {}
