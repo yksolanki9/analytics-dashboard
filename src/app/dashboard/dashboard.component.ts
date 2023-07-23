2433943561a87794f0d273742c5a1e4bde86a27a
yksolanki9 <yashsolanki1709@gmail.com>
2023-07-23 16:58:01 +0530

    Get stats card data from parent

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,6 +11,10 @@ import { Observable } from 'rxjs';
 export class DashboardComponent implements OnInit {
   private breakpointObserver = inject(BreakpointObserver);
 
+  statsBadges: Array<any>;
+
+  barChartData: Object;
+
   cardsLayout$: Observable<any>;
 
   private getCardsLayout(matches: boolean) {
@@ -40,5 +44,51 @@ export class DashboardComponent implements OnInit {
       .pipe(map(({ matches }) => this.getCardsLayout(matches)));
 
     this.cardsLayout$.subscribe((data) => console.log('cela', data));
+
+    this.statsBadges = [
+      {
+        title: 'Badge title',
+        timeline: 'April - 2023 (Till today)',
+        value: 3969,
+        percentChange: -11.0,
+        previousStat: 'March - 2023: 4459',
+      },
+      {
+        title: 'Badge title',
+        timeline: 'April - 2023 (Till today)',
+        value: 3969,
+        percentChange: -11.0,
+        previousStat: 'March - 2023: 4459',
+      },
+      {
+        title: 'Badge title',
+        timeline: 'April - 2023 (Till today)',
+        value: 3969,
+        percentChange: -11.0,
+        previousStat: 'March - 2023: 4459',
+      },
+      {
+        title: 'Badge title',
+        timeline: 'April - 2023 (Till today)',
+        value: 3969,
+        percentChange: -11.0,
+        previousStat: 'March - 2023: 4459',
+      },
+      {
+        title: 'Badge title',
+        timeline: 'April - 2023 (Till today)',
+        value: 3969,
+        percentChange: -11.0,
+        previousStat: 'March - 2023: 4459',
+      },
+    ];
+
+    this.barChartData = {
+      labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+      datasets: [
+        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      ],
+    };
   }
 }
--- src/app/shared/components/stat-card/stat-card.component.ts
+++ src/app/shared/components/stat-card/stat-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stat-card',
   templateUrl: './stat-card.component.html',
-  styleUrls: ['./stat-card.component.scss']
+  styleUrls: ['./stat-card.component.scss'],
 })
 export class StatCardComponent {
+  @Input() timeline: string;
 
+  @Input() value: number;
+
+  @Input() percentChange: number;
+
+  @Input() previousStat: string;
 }
